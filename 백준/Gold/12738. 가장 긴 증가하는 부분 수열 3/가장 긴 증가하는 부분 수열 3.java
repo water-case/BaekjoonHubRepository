36c390c6749c44201852292f3fb3bc24e8ba2d3a
WaterCase <65841586+water-case@users.noreply.github.com>
2022-04-25 07:38:16 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 600 ms, Memory: 169508 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N=Integer.parseInt(br.readLine());
+		int[] nums=new int[N];
+		st=new StringTokenizer(br.readLine()," ");
+		for(int i=0; i<N; i++) {
+			nums[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		int[] lis=new int[N];
+		Arrays.fill(lis, Integer.MAX_VALUE);
+		for(int i=0; i<N; i++) {
+			if(Arrays.binarySearch(lis, nums[i])<0) {
+				lis[-Arrays.binarySearch(lis, nums[i])-1]=nums[i];
+			}
+		}
+		
+		int cnt=0;
+		for(int i=0; i<N; i++) {
+			if(lis[i]==Integer.MAX_VALUE) break;
+			cnt++;
+		}
+		System.out.println(cnt);
+		br.close();
+	}
+	
+}
