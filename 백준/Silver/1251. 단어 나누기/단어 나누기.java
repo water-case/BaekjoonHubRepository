3688e30862d80a99a3bfca89dc0ab6398c15ef99
WaterCase <65841586+water-case@users.noreply.github.com>
2022-09-12 14:04:03 +0900

    [Silver V] Title: 단어 나누기, Time: 88 ms, Memory: 13184 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.java
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.java
@@ -3,31 +3,33 @@ import java.util.*;
 
 public class Main {
 
-	static String str;
+	static String word, res="zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
 	static ArrayList<String> words=new ArrayList<>();
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
-		str=br.readLine();
-		Bruteforce(str,0,0,new String[3]);
-		words.sort((o1, o2)->{ return o1.compareTo(o2); });
+		word=br.readLine();
+		Bruteforce(0,0,new String[3]);
 		
-		System.out.println(words.get(0));
+		System.out.println(res);
 		br.close();
 	}
 
-	private static void Bruteforce(String word, int cnt, int pos, String[] sa) {
+	private static void Bruteforce(int cnt, int pos, String[] sa) {
 		if(cnt==2) {
 			sa[2]=new StringBuffer(word.substring(pos, word.length())).reverse().toString();
 			
-			words.add(sa[0]+sa[1]+sa[2]);
+			String tmp=sa[0]+sa[1]+sa[2];
+			if(res.compareTo(tmp)>0) {
+				res=tmp;
+			}
 			return;
 		}
 		
 		for(int i=pos+1; i<word.length(); i++) {
 			sa[cnt]=new StringBuffer(word.substring(pos, i)).reverse().toString();
-			Bruteforce(word, cnt+1, i, sa);
+			Bruteforce(cnt+1, i, sa);
 		}
 	}
 	
