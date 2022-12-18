d5233c8230e4c132c0cbf77c173972dc294265ef
WaterCase <65841586+water-case@users.noreply.github.com>
2022-12-18 13:49:49 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine())+"\n1");
+        br.close();
+    }
+}
