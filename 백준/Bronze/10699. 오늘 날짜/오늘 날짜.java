e55087a66919f06f4ca0d8963a4a999c6c6e6f6e
WaterCase <65841586+water-case@users.noreply.github.com>
2023-01-20 13:48:50 +0900

    [Bronze V] Title: 오늘 날짜, Time: 116 ms, Memory: 13184 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+    }
+
+}
