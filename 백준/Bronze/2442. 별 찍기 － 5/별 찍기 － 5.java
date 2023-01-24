0bc3a693cd54c3aef25fd3f9c4d93dcfdcc47797
WaterCase <65841586+water-case@users.noreply.github.com>
2023-01-25 07:37:04 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 84 ms, Memory: 11628 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        init(br);
+        solve();
+
+        System.out.println(res);
+        br.close();
+    }
+
+    static String res;
+    static int N;
+
+    private static void solve() {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N-i-1; j++) {
+                sb.append(" ");
+            }
+            for(int j=0; j<2*i+1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        res = sb.toString();
+    }
+
+    private static void init(BufferedReader br) throws Exception {
+        N = Integer.parseInt(br.readLine());
+    }
+
+}
