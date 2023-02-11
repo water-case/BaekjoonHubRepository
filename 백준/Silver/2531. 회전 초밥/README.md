407754baa7de267d2d337e0b20ce9dee70d01d49
WaterCase <65841586+water-case@users.noreply.github.com>
2023-02-11 12:40:15 +0900

    [Silver I] Title: 회전 초밥, Time: 180 ms, Memory: 17252 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/README.md
+++ 백준/Silver/2531. 회전 초밥/README.md
@@ -4,11 +4,11 @@
 
 ### 성능 요약
 
-메모리: 19460 KB, 시간: 152 ms
+메모리: 17252 KB, 시간: 180 ms
 
 ### 분류
 
-브루트포스 알고리즘(bruteforcing), 두 포인터(two_pointer)
+브루트포스 알고리즘(bruteforcing), 슬라이딩 윈도우(sliding_window), 두 포인터(two_pointer)
 
 ### 문제 설명
 
--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,45 +1,64 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
-	static int N,d,k,c,res=-1;
-	static int[] dish,dcnt;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine()," ");
-		
-		N=Integer.parseInt(st.nextToken());
-		d=Integer.parseInt(st.nextToken());
-		k=Integer.parseInt(st.nextToken());
-		c=Integer.parseInt(st.nextToken());
-		
-		dish=new int[N+k-1];
-		dcnt=new int[d+1];
-		for(int i=0; i<N; i++) dish[i]=Integer.parseInt(br.readLine());
-		for(int i=0; i<k-1; i++) dish[N+i]=dish[i];
-		
-		brute(0,0);
-		
-		System.out.println(res);
-		br.close();
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	private static void brute(int idx, int cnt) {
-		if(idx==N+k-1) return;
-		
-		if(dcnt[c]==0) res=Math.max(res, cnt+1);
-		else res=Math.max(res, cnt);
-		
-		if(idx>=k) {
-			if(dcnt[dish[idx-k]]==1) cnt--;
-			dcnt[dish[idx-k]]--;
-		}
-		if(dcnt[dish[idx]]==0) cnt++;
-		dcnt[dish[idx]]++;
-		
-		brute(idx+1, cnt);
-	}
-	
+        init(br);
+        solve();
+
+        System.out.println(res);
+        br.close();;
+    }
+
+    static int res, N, d, k, c;
+    static ArrayList<Integer> belt;
+
+    private static void solve() {
+        int[] dcnt=new int[d+1];
+        int cnt = 0;
+
+        for(int i=0; i<N+k-1; i++) {
+            if(i>=k) {
+                if(dcnt[belt.get(i-k)]==1) {
+                    cnt--;
+                }
+                dcnt[belt.get(i-k)]--;
+            }
+
+            if(dcnt[belt.get(i)]==0) {
+                cnt++;
+            }
+            dcnt[belt.get(i)]++;
+
+            if(dcnt[c]==0) {
+                res=Math.max(res, cnt+1);
+            } else {
+                res=Math.max(res, cnt);
+            }
+        }
+    }
+
+    private static void init(BufferedReader br) throws Exception {
+        res = -1;
+
+        StringTokenizer st=new StringTokenizer(br.readLine()," ");
+        N=Integer.parseInt(st.nextToken()); // 접시개수
+        d=Integer.parseInt(st.nextToken()); // 초밥의 가지수
+        k=Integer.parseInt(st.nextToken()); // 연속해서 먹는 접시의 수
+        c=Integer.parseInt(st.nextToken()); // 쿠폰 번호
+
+        belt = new ArrayList<>();
+        for(int i=0; i<N; i++) {
+            belt.add(Integer.parseInt(br.readLine()));
+        }
+
+        for(int i=0; i<k-1; i++) {
+            belt.add(belt.get(i));
+        }
+    }
+
 }
