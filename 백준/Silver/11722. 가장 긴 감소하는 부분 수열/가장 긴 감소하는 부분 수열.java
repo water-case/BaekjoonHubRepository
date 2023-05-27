7778b7a4b2a1f3b2f38818e1dcdca800aa113390
WaterCase <65841586+water-case@users.noreply.github.com>
2023-05-28 08:20:17 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 100 ms, Memory: 11972 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        nArr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<N; i++) {
+            nArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        br.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+    }
+
+    static int N, nArr[];
+
+    private static void solve() {
+        int res = 0;
+
+        res = dp(N);
+
+        System.out.println(res);
+    }
+
+    private static int dp(int n) {
+        int res = 0;
+
+        int[] memo = new int[n];
+        for(int i=0; i<N; i++) {
+            memo[i] = 1;
+            for(int j=0; j<i; j++) {
+                if(nArr[i]<nArr[j] && memo[i]<memo[j]+1) {
+                    memo[i] = memo[j]+1;
+                }
+            }
+        }
+
+        for(int i : memo) {
+            res = Math.max(res, i);
+        }
+
+
+        return res;
+    }
+
+}
