cc7d3d0a708e17bf94b0d2e2f40377ea6863be16
WaterCase <65841586+water-case@users.noreply.github.com>
2023-05-30 08:13:16 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        br.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+    }
+
+    static int n;
+
+    private static void solve() {
+        long res = 0;
+
+        long[] fibo = dp(n);
+        res = fibo[n];
+
+        System.out.println(res);
+    }
+
+    private static long[] dp(int n) {
+        long[] res = new long[n+3];
+
+        res[1] = 1;
+        res[2] = 1;
+        for(int i=3; i<=n; i++) {
+            res[i] = res[i-1] + res[i-2];
+        }
+
+        return res;
+    }
+
+}
