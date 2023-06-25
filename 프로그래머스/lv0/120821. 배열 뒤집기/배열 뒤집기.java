9498d79eb46e259852c7a774602bfec494a50fed
WaterCase <65841586+water-case@users.noreply.github.com>
2023-06-26 08:44:51 +0900

    [level 0] Title: 배열 뒤집기, Time: 2.26 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        return IntStream.range(0, num_list.length)
+            .map(i -> num_list[num_list.length-1 - i])
+            .toArray();
+    }
+}
