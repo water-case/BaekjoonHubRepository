8eae67c51d5d625971d808f113ca7b2fbaad2568
WaterCase <65841586+water-case@users.noreply.github.com>
2023-07-13 06:16:01 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int a, int b) {
+
+        b /= gcd(a, b);
+        
+        while(b!=1) {
+            if(b%2==0) {
+                b/=2;
+            } else if(b%5==0) {
+                b/=5;
+            } else {
+                return 2;
+            }
+        }
+        
+        return 1;
+    }
+    
+    private int gcd(int a, int b) {
+        if(b==0) {
+            return a;
+        }
+        
+        return gcd(b, a%b);
+    }
+}
