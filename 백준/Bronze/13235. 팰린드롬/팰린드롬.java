cbb6fbe678972a97dc98a891ff0e434c49ece1d8
WaterCase <65841586+water-case@users.noreply.github.com>
2024-01-25 07:30:21 +0900

    [Bronze II] Title: 팰린드롬, Time: 72 ms, Memory: 11448 KB -BaekjoonHub

---
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        System.out.println(input.equals(new StringBuilder(input).reverse().toString()) ? "true" : "false");
+        br.close();
+    }
+
+}
