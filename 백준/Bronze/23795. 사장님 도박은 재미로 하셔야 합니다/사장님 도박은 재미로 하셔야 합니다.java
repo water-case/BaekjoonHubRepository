f5bbf4b9897ed0db814e4727fc93db682d4f2cea
WaterCase <65841586+water-case@users.noreply.github.com>
2024-02-11 12:09:04 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 88 ms, Memory: 11964 KB -BaekjoonHub

---
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int res = 0;
+        String input = "";
+        while(!(input = br.readLine()).equals("-1")) {
+            res += Integer.parseInt(input);
+        }
+
+        System.out.println(res);
+        br.close();
+    }
+
+}
