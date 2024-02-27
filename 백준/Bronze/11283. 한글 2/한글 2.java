62121934b2b4a20b0779f723ddecca39dd6bb427
WaterCase <65841586+water-case@users.noreply.github.com>
2024-02-28 07:21:31 +0900

    [Bronze IV] Title: 한글 2, Time: 72 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char c = br.readLine().charAt(0);
+        System.out.println(c - 44031);
+
+        br.close();
+    }
+
+}
