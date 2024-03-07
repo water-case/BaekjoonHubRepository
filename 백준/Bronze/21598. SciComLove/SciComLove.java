735044161eb250df2bd6ffc7bfa95b02216440cb
WaterCase <65841586+water-case@users.noreply.github.com>
2024-03-08 07:34:25 +0900

    [Bronze V] Title: SciComLove, Time: 72 ms, Memory: 11524 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder res = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        while(N-->0) {
+            res.append("SciComLove").append("\n");
+        }
+
+        System.out.println(res.toString());
+        br.close();
+    }
+
+}
