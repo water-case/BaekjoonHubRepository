ed30d247bdfa8e6dac639ebc54193eda7046300e
WaterCase <65841586+water-case@users.noreply.github.com>
2024-03-13 07:30:56 +0900

    [Bronze V] Title: Hook, Time: 68 ms, Memory: 11272 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("#  # #### #### #  #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#  # #### #### #  #");
+        br.close();
+    }
+
+}
