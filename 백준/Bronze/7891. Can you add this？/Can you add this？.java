c89d1d31b73523bade419d62a1e38746c23638d1
WaterCase <65841586+water-case@users.noreply.github.com>
2024-03-14 08:36:01 +0900

    [Bronze V] Title: Can you add this?, Time: 76 ms, Memory: 11640 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder res = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        while(t-->0) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            res.append(x+y).append("\n");
+        }
+
+        System.out.println(res.toString());
+        br.close();
+    }
+
+}
