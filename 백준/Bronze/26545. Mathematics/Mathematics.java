25374846929510b6efc8360a523be498b6768d82
WaterCase <65841586+water-case@users.noreply.github.com>
2024-03-26 07:25:39 +0900

    [Bronze V] Title: Mathematics, Time: 72 ms, Memory: 11432 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int res = 0;
+        int n = Integer.parseInt(br.readLine());
+        while(n-->0) {
+            res += Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(res);
+        br.close();
+    }
+
+}
