41d1c8e24d81a79239ead81b5bb62a327eafcbf8
WaterCase <65841586+water-case@users.noreply.github.com>
2024-04-02 07:01:25 +0900

    [Bronze V] Title: 라면 공식, Time: 80 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder res = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        while(N-->0) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int X = Integer.parseInt(st.nextToken());
+
+            res.append(A*(X-1) + B).append("\n");
+        }
+
+        System.out.println(res.toString());
+        br.close();
+    }
+
+}
