7d5e6a74542774d706ba304287fff94e041bf4ca
WaterCase <65841586+water-case@users.noreply.github.com>
2024-04-25 07:36:06 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 72 ms, Memory: 11268 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int res = 0;
+        String input = "";
+        while((input = br.readLine()) != null) {
+            if(input.equals("gum gum for jay jay")) {
+                res++;
+            }
+        }
+
+        System.out.println(res);
+        br.close();
+    }
+
+}
