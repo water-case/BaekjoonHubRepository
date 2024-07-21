6311128a2d443a0030ab8077d7cda86b7b550516
WaterCase <65841586+water-case@users.noreply.github.com>
2024-07-22 07:05:39 +0900

    [Bronze III] Title: 이 문제는 D2 입니다., Time: 76 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Bronze/23303. 이 문제는 D2 입니다．/이 문제는 D2 입니다．.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine().toLowerCase().contains("d2") ? "D2" : "unrated");
+        br.close();
+    }
+
+}
