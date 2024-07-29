a5c501800c0363e00eb05f6f41613aede9cb3916
WaterCase <65841586+water-case@users.noreply.github.com>
2024-07-30 07:10:11 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 96 ms, Memory: 13012 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder res = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        // 가로 5칸
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<5; j++) {
+                printAt(res, N);
+            }
+            res.append("\n");
+        }
+
+        // 세로 3칸
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<3; j++) {
+                printAt(res, N);
+                res.append("\n");
+            }
+        }
+
+        // 가로 5칸
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<5; j++) {
+                printAt(res, N);
+            }
+            res.append("\n");
+        }
+
+        System.out.println(res.toString());
+        br.close();
+    }
+
+    private static void printAt(StringBuilder res, int N) {
+        for(int i=0; i<N; i++) {
+            res.append("@");
+        }
+    }
+
+}
